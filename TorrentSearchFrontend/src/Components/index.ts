c2f259720c508c2447a128218e79d17240552fd4
bestinepayyappilly <bestine1234@gmail.com>
2022-05-27 00:53:30 +0530

    removed LimeTorrents

--- TorrentSearchFrontend/src/Components/index.ts
+++ TorrentSearchFrontend/src/Components/index.ts
@@ -1,3 +1,4 @@
 export { default as Brand } from './Brand'
 export { default as Header } from './Header/Header'
 export { default as Searchbar } from './SearchBar/Searchbar'
+export { default as BottomSheet } from './BottomSheet/BottomSheet'
